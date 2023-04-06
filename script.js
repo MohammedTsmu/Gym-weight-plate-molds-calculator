ce6d6f8c07026553d37cd128e6ef68ccbe680500
mohammed qasim <87497268+MohammedTsmu@users.noreply.github.com>
2023-04-06 14:40:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,16 +44,29 @@ function calculate() {
     // Display the thickness of the weight plate in cm
     document.getElementById(
         "result"
-    ).innerHTML = `<p>Weight plate: ${weight}kg </p>
-    <p>Diameter: ${diameter}cm</p>
-    <p> Thickness: ${thickness.toFixed(2)}cm</p>
-    <p>Radius: ${radius} cm</p>
-    <p>Pipe thickness: ${pipeDiementions}cm </p>
-    <p>circumference(Mold Length): ${circumference.toFixed(2)}cm </p>`;
+    ).innerHTML = `<p>Weight plate: <span>${weight} (kg) </span></p>
+    <p>Diameter:<span>${diameter} (cm)</span></p>
+    <p> Thickness: <span>${thickness.toFixed(2)} (cm)</span></p>
+    <p>Radius: <span>${radius} (cm)</span></p>
+    <p>Pipe thickness: <span>${pipeDiementions} (cm)</span></p>
+    <p>Circumference(plate mold length): <span>${circumference.toFixed(2)} (cm)</span></p>`;
 }
 
+// Get the input fields for weight and diameter
+const weightInput = document.getElementById("weight");
+const diameterInput = document.getElementById("diameter");
 
+// Add an event listener to each input field to listen for the "keydown" event
+weightInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        calculate();
+    }
+});
 
-// .innerHTML = `<p>The thickness of a ${weight}kg weight plate</p><p> with a diameter of ${diameter}cm</p><p> is ${thickness.toFixed(
-//     2
-// )}cm. </p><p>radius will be ${radius} cm</p><p> Pipe will be ${pipeDiementions} cm </p>`;
+diameterInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        calculate();
+    }
+});
